649c16321b73872cb0796aac6fa05d686194d981
mrnmcnmglhs <jorgegameplay811@gmail.com>
2026-03-06 16:50:57 -0300

    fazer exercicio 1

---
+++ projeto/app_green_food/android/app/src/main/kotlin/com/banco/app_green_food/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.banco.app_green_food
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ projeto/app_green_food/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Splash_tela());
+}
+
